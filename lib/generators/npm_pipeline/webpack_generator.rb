606802c8dc7eabd6f00e25f55c7f5b7a86c17de4
Ray Farias <rayrfarias@gmail.com>
2017-07-14 11:23:22 -1000

    handle non-default stylesheets and javascripts
    convention 🙋‍♂️

--- lib/generators/npm_pipeline/webpack_generator.rb
+++ lib/generators/npm_pipeline/webpack_generator.rb
@@ -19,10 +19,11 @@ module NpmPipeline
       end
 
       def update_assets
-        append_to_file 'app/assets/stylesheets/application.css',
-          "/*\n *= require webpack/app\n */\n"
-        append_to_file 'app/assets/javascripts/application.js',
-          "//= require webpack/app\n"
+        append_to_file Dir['app/assets/stylesheets/application.*{css,scss,sass}'].first,
+          "/*\n *= require brunch/app\n */\n"
+        
+        append_to_file Dir['app/assets/javascripts/application.js*'].first,
+          "//= require brunch/app\n"
       end
 
       def update_gitignore
